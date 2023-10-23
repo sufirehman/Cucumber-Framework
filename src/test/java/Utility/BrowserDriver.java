10945f8f3cd60ba7c1a822e2a0b88f122846afda
Sufiyan Rehman <sufiyan.rehman@contour-software.com>
2023-10-23 19:55:40 +0500

    updated code

--- src/test/java/Utility/BrowserDriver.java
+++ src/test/java/Utility/BrowserDriver.java
@@ -1,5 +1,57 @@
+//package Utility;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxOptions;
+//
+//public class BrowserDriver {
+//    private static ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
+//
+//    private BrowserDriver() {
+//    }
+//
+//    public static WebDriver getDriver() {
+//        if (driverThreadLocal.get() == null) {
+//            initializeDriver();
+//        }
+//        return driverThreadLocal.get();
+//    }
+//
+//    private static void initializeDriver() {
+//        String browserType = System.getProperty("browser", "chrome");
+//        WebDriver driver;
+//        switch (browserType.toLowerCase()) {
+//            case "chrome":
+//                // Set the system property to use ChromeDriver from resources folder
+//                System.setProperty("webdriver.chrome.driver", BrowserDriver.class.getClassLoader().getResource("resources/drivers/chromedriver.exe").getPath());
+//                driver = new ChromeDriver();
+//                break;
+//            case "firefox":
+//                // Set the system property to use GeckoDriver from resources folder
+//                System.setProperty("webdriver.gecko.driver",
+//                        BrowserDriver.class.getClassLoader().getResource("resources/drivers/geckodriver.exe").getPath());
+//                FirefoxOptions options = new FirefoxOptions();
+//                driver = new FirefoxDriver(options);
+//                break;
+//            default:
+//                throw new IllegalArgumentException("Invalid browser type: " + browserType);
+//        }
+//        driverThreadLocal.set(driver);
+//    }
+//
+//    public static void closeDriver() {
+//        WebDriver driver = driverThreadLocal.get();
+//        if (driver != null) {
+//            driver.quit();
+//            driverThreadLocal.remove();
+//        }
+//    }
+//}
+
 package Utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,11 +76,11 @@ public class BrowserDriver {
         WebDriver driver;
         switch (browserType.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "C:\\Users\\rehmasuf\\Downloads\\chromedriver-win64\\chromedriver.exe");
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", "C:\\Users\\rehmasuf\\Downloads\\geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
                 FirefoxOptions options = new FirefoxOptions();
                 driver = new FirefoxDriver(options);
                 break;
@@ -37,6 +89,7 @@ public class BrowserDriver {
         }
         driverThreadLocal.set(driver);
     }
+
 
     public static void closeDriver() {
         WebDriver driver = driverThreadLocal.get();
