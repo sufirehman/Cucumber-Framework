0ba6dac4c930e02a22df947a4a09eef5a7461b7d
sufirehman <sufirehman12@gmail.com>
2023-10-26 13:51:08 +0500

    updated

--- src/test/java/StepDefinitions/addToCart.java
+++ src/test/java/StepDefinitions/addToCart.java
@@ -57,7 +57,7 @@ public class addToCart {
     @Then("user remove product from shopping cart")
     public void remove_product() {
         cartPage.click_on_remove();
-        test.log(Status.INFO, "Removed product from cart");
+        test.log(Status.INFO, "Removed product from cart12");
     }
     @Then("user close Browser")
     public void close_browser() {
